061734abf5fdd19942fd942a2e1ecaffb5021238
Yuri Murashko <yrik.murashko@gmail.com>
2022-08-19 19:38:56 +0300

    Add button implementation

--- todo.js
+++ todo.js
@@ -1,18 +1,32 @@
 "use strict"
 
 let todo = [
-    {text: "Пообедать", done: true, color:"yellow"},
-    {text: "Сделать практику", done: false},
+    {id: Date.now(), text: "Задание 1", done: true, color:"yellow"},
+    {id: Date.now()+1, text: "Задание 2", done: false},
+    {id: Date.now()+2, text: "Задание 3", done: false},
+    {id: Date.now()+3, text: "Задание 4", done: false},
 ]
+
+let selectedElement=null;
 
 let listTask;
 
 document.addEventListener("DOMContentLoaded", () => {
     listTask = document.getElementById("olist");
-    // listTask.addEventListener("click", event => {
-    //     console.log(event.target);
-    // });
-    listTask.onclick = event => {console.log(event.target);} 
+
+    listTask.addEventListener("click", event => {
+        let li = event.target.closest('li');
+        if (!li) return;
+
+        if (selectedElement === li) {
+            li.classList.remove("elemselected");
+            selectedElement = null;
+        } else {
+            selectedElement?.classList.remove("elemselected");
+            selectedElement = li;
+            li.classList.add("elemselected");
+        }
+    });
 
     render();
 });
@@ -22,22 +36,97 @@ let render = function() {
     listTask.replaceChildren();
     // Fill list
     todo.forEach(task => {
-        let liElement = document.createElement("li");
-        if ("color" in task) liElement.setAttribute("style", `background-color: ${task.color};`);
-        liElement.innerHTML = `<input type="checkbox" ${(task.done)? "checked" : ""}>
-                            <label>${task.text}</label>`;
-        listTask.append(liElement);
+        listTask.append(createLiElement(task));
     });
     
 }
 
-
-let addItem = function() {
-    todo.push({
-        text: prompt("Введите задачу"),
-        done: false,
-        color: null
-    });
-    render();
+let createLiElement = function(task) {
+    let li = document.createElement("li");
+    li.setAttribute("id", `${task.id}`);
+    if ("color" in task) li.setAttribute("style", `background-color: ${task.color};`);
+    li.innerHTML = `<input type="checkbox" ${(task.done)? "checked" : ""}>
+                        <label for="${task.id}">${task.text}</label>`;
+    return li;
 }
 
+let editItem = function() {
+    let inputTask = document.getElementById("inputTask");
+    let inputColor = document.getElementById("inputColor");
+
+    inputTask.removeAttribute("hidden");
+    inputColor.removeAttribute("hidden");
+
+    inputTask.addEventListener("change", function() {
+        console.log(this.value);
+    });
+}
+
+let addItem = function() {
+    let inputTask = document.getElementById("inputTask");
+    let taskToAdd = {
+        id: Date.now(),
+        text: inputTask.value,
+        done: false,
+    };
+
+    if(!taskToAdd.text) return;
+    
+    let li = createLiElement(taskToAdd);
+    if (selectedElement != null) {
+        todo.splice(todo.findIndex(task => task.id == selectedElement.id)+1, 0, taskToAdd);
+
+        li.classList.add("elemselected");
+        selectedElement.after(li);
+
+        selectedElement.classList.remove("elemselected");
+        selectedElement = li;
+        
+    } else {
+        todo.unshift(taskToAdd);
+        listTask.prepend(li);
+    }
+    
+    inputTask.value = '';
+}
+
+let removeItem = function() {
+    if(selectedElement === null) return;
+    todo.splice(todo.findIndex(task => task.id == selectedElement.id), 1);
+    selectedElement.remove();
+    selectedElement = null;
+}
+
+let upItem = function() {
+    if(selectedElement === null) return;
+    // Get task and its index in array
+    let task = todo.find(task => task.id == selectedElement.id);
+    let indexTask = todo.findIndex(task => task.id == selectedElement.id);
+    // Remove task from todo array
+    todo.splice(indexTask, 1);
+    // Insert task in index-1
+    if (indexTask === 0) {
+        todo.splice(todo.length, 0 ,task);
+        listTask.append(selectedElement);
+    } else {
+        todo.splice(indexTask-1, 0, task);
+        selectedElement.after(selectedElement.previousSibling);
+    }
+}
+
+let downItem = function() {
+    if(selectedElement === null) return;
+    // Get task and its index in array
+    let task = todo.find(task => task.id == selectedElement.id);
+    let indexTask = todo.findIndex(task => task.id == selectedElement.id);
+    // Remove task from todo array
+    todo.splice(indexTask, 1);
+    // Insert task in index+1
+    if (indexTask === todo.length) {
+        todo.splice(0, 0 ,task);
+        listTask.prepend(selectedElement);
+    } else {
+        todo.splice(indexTask+1, 0, task);
+        selectedElement.before(selectedElement.nextSibling);
+    }
+}
